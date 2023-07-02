14dfd268059f23e3c9b4c6c57f5cee293de4e186
Tatsuki Terao <tterao@c1r23s4.42tokyo.jp>
2023-07-02 18:31:44 +0900

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:46:25 by tterao            #+#    #+#             */
-/*   Updated: 2023/07/02 18:30:42 by tterao           ###   ########.fr       */
+/*   Updated: 2023/07/02 18:31:01 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,29 @@ Fixed::Fixed() : fixedPointNum(0)
 Fixed::Fixed(const Fixed& obj) : fixedPointNum(obj.getRawBits())
 {
 	std::cout << "Copy constructor called\n";
+}
+
+Fixed::Fixed(const int num)
+{
+	//10
+	// int n = 0b0000000000000000000000000001010;
+	// n = 0b0000000000000000000101000000000;
+	fixedPointNum = num << binaryPoint;
+}
+
+Fixed::Fixed(const float num)
+{
+	//1234.4321f
+	// 123443210000
+	// 123443210000.0
+	//1234.4321f
+
+	//1234.4321f
+	//1234.4321 * (2**8 = 256)
+	//316014.6176
+	//316014
+
+	fixedPointNum = roundf(num * (1 << binaryPoint));
 }
 
 Fixed::~Fixed()
@@ -47,4 +70,20 @@ void	Fixed::setRawBits(int const raw)
 {
 	std::cout << "setRawBits member function called\n";
 	fixedPointNum = raw;
+}
+
+float	Fixed::toFloat() const
+{
+	return static_cast<float>(fixedPointNum) / (1 << binaryPoint);
+}
+
+int	Fixed::toInt() const
+{
+	return fixedPointNum >> binaryPoint;
+}
+
+std::ostream&	operator<<(std::ostream& os, const Fixed &obj)
+{
+	os << obj.toFloat();
+	return (os);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: tterao <tterao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:47:11 by tterao            #+#    #+#             */
-/*   Updated: 2023/06/30 16:38:30 by tterao           ###   ########.fr       */
+/*   Updated: 2023/07/02 18:30:49 by tterao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define FIXED_H
 
 #include <iostream>
+#include <cmath>
 
 class Fixed
 {
@@ -21,12 +22,18 @@ class Fixed
 		int					fixedPointNum;
 		static const int	binaryPoint;
 	public:
-		Fixed();//default constructor
-		~Fixed();//destructor
-		Fixed(const Fixed& obj);//copy constructor
-		Fixed&	operator=(const Fixed& obj);//assignment operator
+		Fixed();
+		Fixed(const int num);
+		Fixed(const float num);
+		~Fixed();
+		Fixed(const Fixed& obj);
+		Fixed&	operator=(const Fixed& obj);
 		int		getRawBits() const;
 		void	setRawBits(int const raw);
+		float	toFloat() const;
+		int		toInt() const;
 };
+
+std::ostream&	operator<<(std::ostream& os, const Fixed &obj);
 
 #endif
